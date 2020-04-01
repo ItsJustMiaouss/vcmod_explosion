1c99c1329bab8cb1513f8928c8fc5eb5d258ca7f
ItsJustMiaouss <59478524+ItsJustMiaouss@users.noreply.github.com>
2020-04-01 17:00:34 +0200

    Add files via upload

---
+++ vcmod_explosion/lua/autorun/server/vcmod_explosion.lua
@@ -0,0 +1,10 @@
+-- Script  by ItsJustMiaouss
+-- Required script: VCMod
+-- Required addon: vFire
+print("[VCMod - Explosion] Script started")
+
+-- When a vehicle explodes
+hook.Add("VC_engineExploded", "CustomHookName", function(ent, silent)
+	print("[VCMod - Explosion] Vehicle set on fire: "..ent:VC_getName()..".")
+	ent:Ignite(2)
+end)
